4c2f645a356279d284a74b85c105deba51ba14a6
yanuarpmbd <cambridge.pambudi@gmail.com>
2023-03-12 18:14:08 +0800

    add widget

---
+++ app/Filament/Widgets/BukuTamu.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Leandrocfe\FilamentApexCharts\Widgets\ApexChartWidget;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use App\Models\BukuTamu as BukuTamuDB;
+use Maatwebsite\Excel\Concerns\ToArray;
+
+class BukuTamu extends ApexChartWidget
+{
+    /**
+     * Chart Id
+     *
+     * @var string
+     */
+    protected static string $chartId = 'bukuTamu';
+
+    /**
+     * Widget Title
+     *
+     * @var string|null
+     */
+    protected static ?string $heading = 'Buku Tamu';
+
+    /**
+     * Chart options (series, labels, types, size, animations...)
+     * https://apexcharts.com/docs/options
+     *
+     * @return array
+     */
+
+    protected static ?int $sort = 3;
+
+    protected static ?string $pollingInterval = null;
+
+    protected function getFilters(): ?array
+    {
+        $years[] = Carbon::now()->year;
+
+        return $years;
+    }
+
+    protected function getOptions(): array
+    {
+        $bulan = array(
+            '1' => 'Januari',
+            '2' => 'Februari',
+            '3' => 'Maret',
+            '4' => 'April',
+            '5' => 'Mei',
+            '6' => 'Juni',
+            '7' => 'Juli',
+            '8' => 'Agustus',
+            '9' => 'September',
+            '10' => 'Oktober',
+            '11' => 'November',
+            '12' => 'Desember',
+        );
+        $now = Carbon::now()->year;
+        foreach ($bulan as $key => $value) {
+            $query = DB::select(DB::raw("SELECT count(*) AS jumlah
+            FROM
+                buku_tamus
+            WHERE
+                MONTH(created_at) = $key AND YEAR(created_at) = $now"));
+            foreach ($query as $q) {
+                if ($key != null) {
+                    $obj = new \stdClass();
+                    $obj = $q->jumlah;
+                    $data_tamu[] = $obj;
+                    $month[] = $value;
+                }
+            }
+        }
+        return [
+            'chart' => [
+                'type' => 'bar',
+                'height' => 400,
+                'toolbar' => [
+                    'show' => false,
+                ],
+            ],
+            'series' => [
+                [
+                    'name' => 'Jumlah Tamu',
+                    'data' => $data_tamu,
+                ],
+            ],
+            'xaxis' => [
+                'categories' => $month,
+                'min' => 0,
+                'floating' => true,
+                'labels' => [
+                    'style' => [
+                        'colors' => '#9ca3af',
+                        'fontWeight' => 600,
+                    ],
+                ],
+            ],
+            'yaxis' => [
+                'labels' => [
+                    'style' => [
+                        'colors' => '#9ca3af',
+                        'fontWeight' => 600,
+                    ],
+                ],
+            ],
+            'colors' => ['#F59E0B'],
+            'plotOptions' => [
+                'bar' => [
+                    'borderRadius' => 10,
+                    'horizontal' => true,
+                ],
+            ],
+            'grid' => [
+                'show' => false,
+            ],
+            'dataLabels' => [
+                'enabled' => false,
+            ],
+            'colors' => ['#facc15', '#38bdf8'],
+            'fill' => [
+                'type' => 'gradient',
+                'gradient' => [
+                    'shade' => 'dark',
+                    'type' => 'horizontal',
+                    'shadeIntensity' => 0.5,
+                    'gradientToColors' => ['#f43f5e'],
+                    'inverseColors' => false,
+                    'opacityFrom' => 1,
+                    'opacityTo' => 1,
+                    'stops' => [0, 100],
+                ],
+            ],
+        ];
+    }
+}
