fa0dabff4e6f2a45f6938485a8c23c1e3a04a981
yanuarpmbd <cambridge.pambudi@gmail.com>
2023-06-04 01:29:36 +0800

    custom login

--- config/filament.php
+++ config/filament.php
@@ -90,7 +90,7 @@ return [
     'auth' => [
         'guard' => env('FILAMENT_AUTH_GUARD', 'web'),
         'pages' => [
-            'login' => \Filament\Http\Livewire\Auth\Login::class,
+            'login' => App\Http\Livewire\Auth\Login::class,
         ],
     ],
 
